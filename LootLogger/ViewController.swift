5fd34d131cfd9e84965e2e9234e226cbedde20d0
Sami Bajwa <samibajwa@csu.fullerton.edu>
2023-05-09 15:50:44 -0700

    Initial Commit

---
+++ LootLogger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LootLogger
+//
+//  Created by Sami Bajwa on 5/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
